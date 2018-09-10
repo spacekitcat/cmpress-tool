89e634edb29baaf613295a1529e2710d5e871d05
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-09-10 15:07:02 +0100

    Adding sliding window test skeleton

---
+++ __tests__/sliding-window.test.spec.js
@@ -0,0 +1,9 @@
+import { SlidingWindow } from '../src/sliding-window';
+
+describe('SlidingWindow', () => {
+  /** This is a useless test. It's here as a skeleton for the SlidingWindow unit tests. */
+  it('the slidingWindow instance is not null', () => {
+    let slidingWindow = new SlidingWindow();
+    expect(slidingWindow).not.toEqual(undefined);
+  });
+});
