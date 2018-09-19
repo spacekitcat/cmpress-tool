9980e83aa79490000878b2ea0812dd73679d2a02
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-09-19 01:00:29 +0100

    Adding a test program for compressing files. Pretty pleased at how it copes with real files at this stage.

---
+++ samples/filecompresssimulate.js
@@ -0,0 +1,80 @@
+#!/usr/bin/env node
+import { CompressorTransformer } from '../lib/compressor-transformer';
+
+import fs from 'fs';
+
+let dictionaryLen = 32;
+let windowLen = 32;
+
+let calculateReductionPercentage = (compressedPackets, originalInput) =>
+  ((compressedPackets.length / originalInput.length) * 100).toFixed(2);
+
+let extractCompressedPacketsTokenString = compressedPackets =>
+  compressedPackets
+    .map(item => {
+      return item.token;
+    })
+    .join('');
+
+let filePath = process.argv[2];
+
+if (!filePath) {
+  console.log('No input file path specified. Please provide a file path.');
+  process.exit(-1);
+}
+
+let fileReadStream = fs.createReadStream(filePath, 'utf8');
+let data = '';
+
+let compressorTransformer = new CompressorTransformer({
+  objectMode: true
+});
+
+let rawBytes;
+fileReadStream.on('data', input => {
+  rawBytes += input;
+});
+
+fileReadStream.pipe(compressorTransformer);
+
+let outputAccumulator = [];
+let alt = 0;
+compressorTransformer.on('data', compressedPacket => {
+  if (alt >= 10000) {
+    alt = 0;
+  } else if (alt >= 5000) {
+    process.stdout.write(
+      '   ->MUST<-   ->COMPRESS<-   ' +
+        calculateReductionPercentage(outputAccumulator, rawBytes) +
+        '    🦊  🦊  🦊  🦊  🦊                         ' +
+        ' \r'
+    );
+    alt++;
+  } else {
+    process.stdout.write(
+      '   ->MUST<-   ->COMPRESS<-   ' +
+        calculateReductionPercentage(outputAccumulator, rawBytes) +
+        '      🐱  🐱  🐱  🐱  🐱                                       ' +
+        ' \r'
+    );
+    alt++;
+  }
+  outputAccumulator.push(compressedPacket);
+});
+
+compressorTransformer.on('end', () => {
+  process.stdout.write(
+    '   ->MUST<-   ->COMPRESS<-   ' +
+      calculateReductionPercentage(outputAccumulator, rawBytes) +
+      '' +
+      '\n'
+  );
+});
+
+fileReadStream
+  .on('data', function(chunk) {
+    data += chunk;
+  })
+  .on('end', function() {
+    //console.log(data);
+  });
