a727b48775ee004fd195927438015a623e30a2b0
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-11-18 18:38:28 +0000

    Simplifying deserialize method.

--- src/deserialize-packet-from-binary.js
+++ src/deserialize-packet-from-binary.js
@@ -2,6 +2,18 @@ const invalidFatalInput = input => typeof input !== 'string' || !input;
 
 const hasPrefixField = input => input.length >= 5 && input[1] === 'P';
 
+const readPrefixValue = field => {
+  let prefixValues = field.split(',');
+  let prefixValue1 = parseInt(prefixValues[0]);
+  let prefixValue2 = parseInt(prefixValues[1]);
+
+  if (isNaN(prefixValue1) || isNaN(prefixValue2)) {
+    throw new Error('Invalid compression serialization stream prefix value');
+  }
+
+  return [prefixValue1, prefixValue2];
+};
+
 const deserializePacketFromBinary = serializedString => {
   if (invalidFatalInput(serializedString)) {
     throw new Error('Error: Invalid compression serialisation stream format.');
@@ -10,15 +22,8 @@ const deserializePacketFromBinary = serializedString => {
   let output = { t: serializedString[0] };
 
   if (hasPrefixField(serializedString)) {
-    let prefix = serializedString
-      .substr(2, serializedString.length - 1)
-      .split(',');
-    let prefixValue1 = parseInt(prefix[0]);
-    let prefixValue2 = parseInt(prefix[1]);
-    if (isNaN(prefixValue1) || isNaN(prefixValue2)) {
-      throw new Error('Invalid compression serialization stream prefix value');
-    }
-    output.p = [prefixValue1, prefixValue2];
+    let fieldValue = serializedString.substr(2, serializedString.length - 1);
+    output.p = readPrefixValue(fieldValue);
   } else if (serializedString.length >= 2) {
     throw new Error('Error: Invalid compression serialisation stream field.');
   }
