71f4fcc5f87844f8fd285e42360a3cc1239708b7
Lisa Burton <Lisa.Burton01@bbc.co.uk>
2018-12-29 15:54:44 +0000

    Refactor decompress for efficiency and source code enenomy.

--- src/decompressor-transformer.js
+++ src/decompressor-transformer.js
@@ -10,11 +10,37 @@ class DecompressorTransformer extends Transform {
     this.expectingNewToken = true;
     this.missingPackets = 0;
     this.buffer = Buffer.from([]);
-    this.last_chunk = null;
+  }
+
+  expandPacket(packet) {
+    if (packet.p) {
+      let startPosition = this.history_buffer.buffer.length - (packet.p[0] - 1);
+      let result = this.history_buffer.buffer.slice(
+        startPosition - packet.p[1],
+        startPosition
+      );
+
+      for (let i = 0; i < result.length; ++i) {
+        this.history_buffer = consumeInput(
+          this.history_buffer.buffer,
+          this.historyBufferSize,
+          result[i]
+        );
+        this.push(result[i]);          
+      }
+      this.push(packet.t);
+    } else {
+      this.push(packet.t);
+    }
+
+    this.history_buffer = consumeInput(
+      this.history_buffer.buffer,
+      this.historyBufferSize,
+      packet.t
+    );
   }
 
   _transform(chunk, encoding, callback) {
-    let packets = [];
     let currentChunkPointer = 0;
     while (currentChunkPointer < chunk.length) {
       if (this.expectingNewToken) {
@@ -34,41 +60,12 @@ class DecompressorTransformer extends Transform {
 
       if (this.missingPackets === 0) {
         let packet = deserializePacketFromBinary(this.buffer);
-        packets.push(packet);
+        this.expandPacket(packet);
         this.expectingNewToken = true;
         this.buffer = Buffer.from([]);
       }
     }
 
-    packets.forEach(packet => {
-      if (packet.p) {
-        let startPosition = this.history_buffer.buffer.length - (packet.p[0] - 1);
-        let result = this.history_buffer.buffer.slice(
-          startPosition - packet.p[1],
-          startPosition
-        );
-
-        for (let i = 0; i < result.length; ++i) {
-          this.history_buffer = consumeInput(
-            this.history_buffer.buffer,
-            this.historyBufferSize,
-            result[i]
-          );
-          this.push(result[i]);          
-        }
-        this.push(packet.t);
-      } else {
-        this.push(packet.t);
-      }
-
-      this.history_buffer = consumeInput(
-        this.history_buffer.buffer,
-        this.historyBufferSize,
-        packet.t
-      );
-    });
-
-    this.last_chunk = chunk;
     callback();
   }
 }
